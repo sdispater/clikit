3254758bc1cce7000916c8f9201539e638cce181
Sébastien Eustace <sebastien@eustace.io>
2018-12-06 09:55:05 -0500

    Add questions components

---
+++ tests/ui/components/test_question.py
@@ -0,0 +1,72 @@
+import os
+import subprocess
+
+import pytest
+
+from clikit.ui.components import Question
+
+
+def has_tty_available():
+    devnull = open(os.devnull, "w")
+    exit_code = subprocess.call(["stty", "2"], stdout=devnull, stderr=devnull)
+
+    return exit_code == 0
+
+
+TTY_AVAILABLE = has_tty_available()
+
+
+def test_ask(io):
+    question = Question("What time is it?", "2PM")
+    io.set_input("\n8AM\n")
+
+    assert "2PM" == question.ask(io)
+
+    io.clear_error()
+
+    assert "8AM" == question.ask(io)
+    assert "What time is it? " == io.fetch_error()
+
+
+@pytest.mark.skipif(
+    not TTY_AVAILABLE, reason="`stty` is required to test hidden response functionality"
+)
+def test_ask_hidden_response(io):
+    question = Question("What time is it?", "2PM")
+    question.hide()
+    io.set_input("8AM\n")
+
+    assert "8AM" == question.ask(io)
+    assert "What time is it? " == io.fetch_error()
+
+
+def test_ask_and_validate(io):
+    error = "This is not a color!"
+
+    def validator(color):
+        if color not in ["white", "black"]:
+            raise Exception(error)
+
+        return color
+
+    question = Question("What color was the white horse of Henry IV?", "white")
+    question.set_validator(validator)
+    question.set_max_attempts(2)
+
+    io.set_input("\nblack\n")
+    assert "white" == question.ask(io)
+    assert "black" == question.ask(io)
+
+    io.set_input("green\nyellow\norange\n")
+
+    with pytest.raises(Exception) as e:
+        question.ask(io)
+
+    assert error == str(e.value)
+
+
+def test_no_interaction(io):
+    io.set_interactive(False)
+
+    question = Question("Do you have a job?", "not yet")
+    assert "not yet" == question.ask(io)
